218bb400620332baa712cfd66345e69358f6ee73
subash <sub.paudel@gmail.com>
2020-06-11 21:35:16 +0545

    kasndakkbkjkjdckdcjbbkjkkajnslkas

--- Api/server.js
+++ Api/server.js
@@ -14,9 +14,9 @@ const express = require('express'),
     
 
     const app = express();
-    app.use(express.static(__dirname + "build")); //
+    app.use(express.static(__dirname + "build")); 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", index.html)); // <- try "index.html"
+  res.sendFile(path.resolve(__dirname, "build", "index.html")); 
 });
     app.use(bodyParser.json());
     app.use(cors());
