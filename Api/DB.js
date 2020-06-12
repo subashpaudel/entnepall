a6d5b6ccd51ad0a28496ef569cab9b57fd74a93a
subash <sub.paudel@gmail.com>
2020-06-12 22:19:28 +0545

    f

--- Api/DB.js
+++ Api/DB.js
@@ -1,3 +1,3 @@
 module.exports = {
-    DB: 'mongodb+srv://subash:Ratnamaya1@@cluster0-psl7u.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    DB: 'mongodb+srv://subash:Ratnamaya1@@cluster0-psl7u.mongodb.net/test?retryWrites=true&w=majority'
   };
